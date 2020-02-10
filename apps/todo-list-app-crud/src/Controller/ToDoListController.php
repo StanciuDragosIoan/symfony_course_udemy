6e686e38f05cef0f092edd1beaf3ac90fd7516f5
StanciuDragosIoan <stanciudragosioan92@gmail.com>
2020-02-10 07:30:01 +0200

    test

--- apps/todo-list-app-crud/src/Controller/ToDoListController.php
+++ apps/todo-list-app-crud/src/Controller/ToDoListController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ToDoListController extends AbstractController
@@ -18,10 +20,53 @@ class ToDoListController extends AbstractController
 
 
      /**
-     * @Route("/create", name="create_task")
+     * @Route("/create", name="create_task", methods={"POST"})
      */
-    public function create()
+    public function create(Request $request)
     {
-         exit('todo: create a new task');
+
+        $title = trim($request->request->get('title'));
+
+        if(empty($title)){
+            return $this->redirectToRoute('to_do_list');
+        } else {
+            exit($title);
+        }
+
+        $entityManager = $this->getDoctrine->getManager();
+
+        $task = new Task();
+
+        $task->setTitle($title);
+        //prepare
+        $entityManager->persist($task);
+    
+        //execute
+        $entityManager->flush();
+
+        
+
+        return $this->redirectToRoute('to_do_list');
+    
+    }
+
+
+    /**
+     * @Route("/switch-status/{id}", name="switch_status")
+     */
+    public function switchStatus($id)
+    {   
+         
+        exit('todo: switch the status of a taks with the id ' . $id);
+    }
+
+
+    /**
+     * @Route("/delete/{id}", name="task_delete")
+     */
+    public function delete($id)
+    {   
+         
+        exit('todo: delete a taks with the id ' . $id);
     }
 }
---
+++ course_redone_better_notes_structure/notes.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+
+    ### Temporary global Notes
+
+    video 114:
+        
+        defined create() method in the controller
+        added name for the route and defined what happes in the function
+
+        in the template (index.html.twig):
+            set the action="{{path('create_task')}}" of the form to the route name
+
+        # the controller method works
+
+        set the method of the form to POST and added it to the controller method as well:
+                methods={"POST"}
+
+
+
+*/
